b1bd5167adcb392073c5ca835133149f1d7fe2e3
Mukesh Rawat <70265954+1mukeshr@users.noreply.github.com>
2025-08-26 13:36:33 +0530

    reacfile

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './component/Header';
 import Home from './pages/Home';
-import About from './pages/About';
-import Product from './pages/Product';
 import { CartProvider } from './context/CartContext';
 import Footer from './component/Footer';
-import Categories from './pages/Categories';
 import Fragrances from './category/Fragrances';
 import Beauty from './category/Beauty';
 import Furiture from './category/Furniture';
@@ -23,10 +20,7 @@ function App() {
           <Route path="/fragrancecollection" element={<Fragrances />} />
           <Route path="/beatycollection" element={<Beauty/>} />
           <Route path="/furniturecollection" element={<Furiture/>} />
-          <Route path="/grocerycollection" element={<Groceries/>} />
-          <Route path="/about" element={<About />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/category" element={<Categories />}></Route>
+          <Route path="/grocerycollection" element={<Groceries/>} />  
           <Route path="/checkout" element={<CheckOut />}/>
         </Routes>
         <Footer />
